06146f0a9e8766d512d7a7ba6dbfec209bf10d2b
wes <wesley.l.hightower@gmail.com>
2021-07-08 16:23:59 -0500

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
-import * as Animatable from 'react-native-animatable';
+import React, { Component } from "react";
+import { ScrollView, Text } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 
 class Contact extends Component {
 	constructor(props) {
@@ -12,15 +13,35 @@ class Contact extends Component {
 		title: "Contact Us"
 	};
 
+	sendMail() {
+		MailComposer.composeAsync({
+			recipients: ["wesley.l.hightower@gmail.com"],
+			subject: "Inquiry",
+			body: "How do you sleep at night?"
+		});
+	}
+
 	render() {
 		return (
 			<ScrollView>
-				<Card title='Contact Information' wrapperStyle={{margin:20}}>
+				<Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
 					<Text>1 Nucamp Way</Text>
 					<Text>Seattle, WA 98001</Text>
-					<Text style={{marginBottom:10}}>U.S.A.</Text>
+					<Text style={{ marginBottom: 10 }}>U.S.A.</Text>
 					<Text>Phone: 1-206-555-1234</Text>
 					<Text>Email: campsites@nucamp.co</Text>
+					<Button
+						title="Send Email"
+						buttonStyle={{ backgroundColor: "#5637dd", margin: 40 }}
+						icon={
+							<Icon
+								name="envelope-o"
+								type="font-awesome"
+								color="#fff"
+								iconStyle={{ marginRight: 10 }}
+							/>}
+						onPress={() => this.sendMail()}
+					/>
 				</Card>
 			</ScrollView>
 		);
